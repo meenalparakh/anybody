b591d76ca952e8c6ad02ac2fda4d4946dc6332fe
Meenal Parakh <meenalparakh18@gmail.com>
2025-09-07 19:29:18 -0400

    remove experiment scripts

--- run_experiments/eval_intra_simple_bot_reach.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./docker/cluster/submit_job_ionic.sh test4 /n/fs/pvl-exptrack/anybody scripts/run.py --headless --enable_cameras OVERRIDE_CFGNAME experiment_cfgs/eval_mt.yaml EVAL_CHECKPOINT LOGS_PATH/intra_simple_bot_reach/Tr0-c0-s0-t-nt-h0_42/checkpoints/agent_100000.pt
